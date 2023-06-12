5558a541b64612ab89e2ff723abc20dfe6757484
manonGalle <manon_galle@hotmail.com>
2023-06-12 10:26:48 +0200

    add dynamic pseudo into globalcards

--- src/components/GlobalOverview/GlobalCard/GlobalCard.js
+++ src/components/GlobalOverview/GlobalCard/GlobalCard.js
@@ -1,11 +1,11 @@
-import facebook from "../../../assets/logos/icon-facebook.svg";
+
 import up from "../../../assets/icon/icon-up.svg";
 import down from "../../../assets/icon/icon-down.svg";
 
 import "./GlobalCard.css";
 import { useSelector } from "react-redux";
 
-function GlobalCard() {
+function GlobalCard({ socialLogo, pseudo }) {
 
     const darkmode = useSelector((state) => state.darkMode);
 
@@ -14,9 +14,10 @@ function GlobalCard() {
 
     return(
         <div className={cssClass}>
-            CARD with dynamic props
-            <img src={facebook}/>
-            <h4>pseudo</h4>
+            <div className="pseudo">
+                <img src={socialLogo}/>
+                <h4>@{pseudo}</h4>
+            </div>
             <p><span className="followers-number">##</span> followers</p>
             <p><img src={up}/> {/*add a condition if increase display green up icon and if decrease display red down icon*/} <span className="followers-today-number">##</span> Today </p>
         </div>
--- src/components/GlobalOverview/GlobalOverview.js
+++ src/components/GlobalOverview/GlobalOverview.js
@@ -1,14 +1,22 @@
 import GlobalCard from "./GlobalCard/GlobalCard";
+import facebook from "../../assets/logos/icon-facebook.svg";
+import instagram from "../../assets/logos/icon-instagram.svg";
+import twitter from "../../assets/logos/icon-twitter.svg";
+import youtube from "../../assets/logos/icon-youtube.svg";
 
 import "./GlobalOverview.css";
 
 function GlobalOverview() {
+    const facebookPseudo = "ManonGalle";
+    const twitterPseudo = "ManonG";
+    const instagramPseudo = "MGalle";
+    const youtubePseudo = "MaGalle"
     return (
         <div className="GlobalOverview">
-            <GlobalCard/>
-            <GlobalCard/>
-            <GlobalCard/>
-            <GlobalCard/>
+            <GlobalCard socialLogo={facebook} pseudo={facebookPseudo}/>
+            <GlobalCard socialLogo={instagram} pseudo={instagramPseudo}/>
+            <GlobalCard socialLogo={twitter} pseudo={twitterPseudo}/>
+            <GlobalCard socialLogo={youtube} pseudo={youtubePseudo}/>
         </div>
     )
 }
